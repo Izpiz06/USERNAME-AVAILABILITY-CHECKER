333e9afaa8cdf6d5174b2e7d1c533b72097066ea
Izpiz06 <mdizaan1192@gmail.com>
2026-02-27 03:48:54 +0530

    Update UC1 logic

--- src/UseCase1BaseSetup.java
+++ src/UseCase1BaseSetup.java
@@ -1,19 +1,15 @@
 import java.util.HashMap;
 import java.util.Map;
 
-public class UsernameAvailabilityChecker {
-
+public class UseCase1BaseSetup {
     protected Map<String, Integer> users = new HashMap<>();
-
-    public UsernameAvailabilityChecker() {
+    public UseCase1BaseSetup() {
         users.put("john_doe", 1);
         users.put("admin", 2);
     }
-
     public static void main(String[] args) {
-        UsernameAvailabilityChecker checker =
-                new UsernameAvailabilityChecker();
-
+        UseCase1BaseSetup checker =
+                new UseCase1BaseSetup();
         System.out.println("System Initialized");
     }
 }
